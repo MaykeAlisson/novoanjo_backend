2a8066610853de7c00a7ca72df48ce2ce7946f8e
maykealisson <maykealison@gmail.com>
2022-03-24 15:56:58 -0300

    feat: create models

--- src/main/java/br/com/novoanjo/novoanjo/domain/model/Address.java
+++ src/main/java/br/com/novoanjo/novoanjo/domain/model/Address.java
@@ -1,6 +1,30 @@
 package br.com.novoanjo.novoanjo.domain.model;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "address")
 public class Address implements Serializable {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "zip_code", nullable = false, length = 3)
+    private String zipCode;
+    private String logradouro;
+    private String complement;
+    private Long number;
+    private String state;
+
 }
