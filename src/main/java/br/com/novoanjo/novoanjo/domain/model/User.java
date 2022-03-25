45724530a80190335aa1fb7fc14de07db4d576b2
maykealisson <maykealison@gmail.com>
2022-03-24 21:57:03 -0300

    feat: create models

--- src/main/java/br/com/novoanjo/novoanjo/domain/model/User.java
+++ src/main/java/br/com/novoanjo/novoanjo/domain/model/User.java
@@ -1,6 +1,13 @@
 package br.com.novoanjo.novoanjo.domain.model;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class User implements Serializable {
+
+    private Long id;
+    private String name;
+    private Profile profile;
+    private Phone phone;
+    private Address address;
 }
